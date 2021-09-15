786a5313675f239c9ca20c4a7fcb1d24c3fd4382
Dollnkey <71886077+DollnKey@users.noreply.github.com>
2021-09-15 01:43:48 -0300

    Arquivo de exemplo!!

---
+++ examples/Index.js
@@ -0,0 +1,25 @@
+const { Client, Intents } = require('discord.js')
+const client = new Client({ ws: { intents: Intents.ALL } })
+const getRadioStations = require('DollnKey/hunter.fm');
+
+client.on('ready', async () => {
+    console.log('Online!')
+});
+
+client.on('message', async message => {
+    if(message.channel.type === 'dm' || message.author.bot) return;
+    if(message.content === '/station') {
+        if (message.member.voice.channel) {
+            const connection = await message.member.voice.channel.join();
+            getRadioStations()
+            .then(stations => {
+                var music = stations[0].strems.lowQuality
+                connection.play(music)
+            }).catch(console.error);
+        } else {
+            message.channel.send(':x: Você não está em um canal de Voz!!');
+        }
+    }
+});
+
+client.login('Token aqui!!')
